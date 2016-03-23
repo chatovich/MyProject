99d8fe80096b38ac44b1c3ca897ca472a5f26d74
chatovich <chatovich@gmail.com>
2016-03-23 12:24:46 +0300

    xsd

--- src/by/it/Aperture.java
+++ src/by/it/Aperture.java
@@ -6,27 +6,27 @@ package by.it;
 public class Aperture {
     Double width;//ширина проема
     Double height;//высота проема
-    Integer amount;//количество проемов
+    Integer count;//количество проемов
     String typeOfAperture;//тип дверь\окно\технолог.проем
     Double squareOfAperture;//площадь проема
     /**
      * Конструктор объекта проема с указанием количества проемов
      */
-    public Aperture(String name, String width, String height, String amount){
+    public Aperture(String name, String width, String height, String count){
         this.width=Double.valueOf(width);
         this.height=Double.valueOf(height);
-        this.amount=Integer.valueOf(amount);
+        this.count=Integer.valueOf(count);
         this.typeOfAperture=name;
         squareOfAperture=this.height*this.width;//площадь проема
     }
     /**
      * Конструктор объекта проема без указанием количества проемов (стандартно 1)
      */
-    public Aperture(String name, String width, String height){
+    public Aperture(String name, String width, String height, String count, String square){
         this.width=Double.valueOf(width);
         this.height=Double.valueOf(height);
-        this.amount=1;
+        this.count=Integer.valueOf(count);
         this.typeOfAperture=name;
-        squareOfAperture=this.height*this.width;//площадь проема
+        squareOfAperture=Double.valueOf(square);//площадь проема
     }
 }
